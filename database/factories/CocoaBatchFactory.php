84b1f441f027415a8b8cbaa918a5e741fa236b78
Guilherme-Ferreti <guiferretis@gmail.com>
2021-07-05 21:03:37 -0300

    added CocoaBatch factory

---
+++ database/factories/CocoaBatchFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\CocoaBatch;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CocoaBatchFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = CocoaBatch::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'description' => $this->faker->words(8, true),
+            'provider' => CocoaBatch::PROVIDERS[array_rand(CocoaBatch::PROVIDERS)],
+            'origin' => CocoaBatch::ORIGINS[array_rand(CocoaBatch::ORIGINS)],
+        ];
+    }
+}
